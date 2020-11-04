8282fca52f05e39d7ff42cf8239a9a44ca8d7f20
Denox9 <kierwinramos28@gmail.com>
2020-11-04 19:47:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-s1jazi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-s1jazi)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+}
